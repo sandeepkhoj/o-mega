1fa14a5c984a89e67ad3181dcb9e8992cf1d8d22
Sandeep Yadav <sandeepkhoj@gmail.com>
2015-08-03 17:39:48 +0530

    init

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
